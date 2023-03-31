55daa6b6417744803621f831ae29ccf3f5121e62
Felipe Barbosa <fekipedmb@gmail.com>
2023-03-30 23:34:14 -0300

    Funcionalidade: Desligamento suave

--- README.md
+++ README.md
@@ -2,22 +2,36 @@
 
 Servidor da nossa aplicação. Tecnologias usadas:
 
-- [Node (18.5.0 - LTS)](https://nodejs.org/en/download)
+- [Node (18.15.0 - LTS)](https://nodejs.org/en/download)
 - NPM (9.5.0)
-- ExpressJS
-- TypeScript
-- Prisma
+- ExpressJS (4.18)
+- TypeScript (5.0)
+- Prisma (4.12)
 
 ## Instalação
 
 Primeiro realize o `git clone` :
 
 ```sh
-git@github.com:PJI110-SALA-001GRUPO-009-2023/read-together-server.git
+git clone git@github.com:PJI110-SALA-001GRUPO-009-2023/read-together-server.git
 ```
 
-Em seguida:
+Em seguida, acesse a raiz do projeto:
+
+```sh
+cd read-together-server
+```
+
+E rode a instalação:
 
 ```sh
 npm install
+```
+
+## Rodando a Aplicação (Desenvolvimento)
+
+Para rodar a aplicação, na raiz do projeto:
+
+```sh
+npm run dev
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,52 @@
 import express from 'express'
 import * as dotenv from 'dotenv'
+import { IncomingMessage, Server, ServerResponse } from 'http'
 
 dotenv.config()
 
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello Wold!')
+    res.send('Olá mundo!')
 })
 
 const port = Number(process.env.NODE_PORT) || 8080
 
-app.listen(port, () => {
-    console.log('Listen to port: ' + port)
+const servidor = app.listen(port, () => {
+    console.log('Escutando porta: ' + port)
 })
+
+// TODO refatorar desligamento suave (graceful shutdown) 
+function lidarFecharServidor(this: Server<typeof IncomingMessage, typeof ServerResponse>):void {
+    console.log('Fechando servidor')
+    this.closeAllConnections()
+    this.closeIdleConnections()
+}
+
+servidor.on('close', lidarFecharServidor)
+
+// HOF - high order function
+function hofDesligamentoSuave(servidor: Server<typeof IncomingMessage, typeof ServerResponse>): () => Promise<void> {
+    async function lidar(): Promise<void> {
+        console.log('Sinal de desligamento recebido ', new Date(Date.now()).toISOString())
+        try {
+            await servidor.close((err) => {
+                if (err != undefined) {
+                    console.error('Erro ao fechar servidor: ', err)
+                }
+                console.log('Servidor fechado')
+            })
+        } catch(e) {
+            console.error('Erro ', e)
+        } finally {
+            console.log('App desligado', new Date(Date.now()).toISOString())
+            process.exit()
+        }
+    }
+    return lidar
+}
+
+const lidarDeslgamento = hofDesligamentoSuave(servidor)
+
+process.on('SIGINT', lidarDeslgamento)
+process.on('SIGTERM', lidarDeslgamento)
